bf5a362b36e20a8fe21cd4f3368676e6e9f53382
IsaíasCR <102234828+IsaiasCR07@users.noreply.github.com>
2023-09-18 19:55:35 -0600

    Update mi-archivo.js
    Colocacion del proyecto y gist

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,2 +1,6 @@
 Alert("Hola Mundo_editado");
 Console.log("cambios en el archivo");
+
+#https://github.com/users/IsaiasCR07/projects/1/views/2
+#https://gist.github.com/IsaiasCR07/5c795de13e0d39cb0893690535300a96 
+  
